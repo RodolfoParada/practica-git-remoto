9ff4125f7b9c26f077d1280e8a462b6e7dddc162
Rodolfo Parada <rodolfo.parada.gonzalez>
2025-11-05 15:12:42 -0300

    feat: agrega funcion nombreVehiculo

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("¡Hola desde GitHub!")
+
+// Función de saludo personalizado
+function nombreVehiculo(nombre) {
+    console.log(`¡Este es un excelente modelo ${nombre} de vehículo!`);
+}
+
+// Uso de la función
+nombreVehiculo("Toyota Corolla");
