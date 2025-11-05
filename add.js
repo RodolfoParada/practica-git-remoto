6cc5fec0be01ba16dbc4ca7f2a65a5e2d08e8d15
Rodolfo Parada <rodolfo.parada.gonzalez>
2025-11-05 13:50:11 -0300

    feat: agregar función de saludo personalizado

--- add.js
+++ add.js
@@ -1,1 +1,8 @@
 console.log("¡Hola desde GitHub!")
+
+// Función de saludo personalizado
+function saludarUsuario(nombre) {
+    console.log(`¡Hola, ${nombre}! Bienvenido a nuestro proyecto.`);
+}
+
+saludarUsuario("Desarrollador");
